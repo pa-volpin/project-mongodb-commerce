12c4450ada1343d68cfbe143255e6ce36d95e925
pa-volpin <paulo.volpin@gmail.com>
2021-01-24 11:46:46 -0300

    Correction on task 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,8 +1,8 @@
-db.produtos.updateMany({},
-  { $push: { vendasPorDia: {
-    $each: [NumberInt(0), NumberInt(0), NumberInt(0), NumberInt(0),
-      NumberInt(0), NumberInt(0), NumberInt(0)] } } },
-  { $upsert: true });
+db.produtos.updateMany({}, { $push: { vendasPorDia:
+  { $each: [NumberInt(0), NumberInt(0), NumberInt(0), NumberInt(0),
+    NumberInt(0), NumberInt(0), NumberInt(0)] } } }, { $upsert: true });
 db.produtos.updateOne({ nome: "Big Mac" },
   { $inc: { "vendasPorDia.3": NumberInt(60) } });
+db.produtos.updateOne({ $and: [{ tags: "bovino" }, { tags: "pão" }] },
+  { $inc: { "vendasPorDia.6": NumberInt(120) } });
 db.produtos.find({}, { nome: 1, vendasPorDia: 1, _id: 0 });
