a979ccff407532748bae78e22263e24a34762c3d
pa-volpin <paulo.volpin@gmail.com>
2021-01-25 00:59:39 -0300

    Tasks 13 and 14

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,15 +1,9 @@
 db.produtos.updateMany(
-  {},
+  { valoresNutricionais: {
+    $elemMatch: { tipo: "sódio",
+      $and: [{ percentual: { $gt: 20 } }, { percentual: { $lt: 40 } }],
+    },
+  } },
   { $push: { tags: "contém sódio" } },
-  { arrayFilters: [{
-    $and: [
-      { "valororesNutricionais.$[elemento].tipo": "sódio" },
-      { $and: [
-        { "valororesNutricionais.$[elemento].percentual": { $gt: 20 } },
-        { "valororesNutricionais.$[elemento].percentual": { $lt: 40 } },
-      ] },
-    ],
-  }] },
 );
-
 db.produtos.find({}, { nome: 1, tags: 1, _id: 0 });
